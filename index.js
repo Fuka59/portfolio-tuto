a299f179b17176ec2701f9f82dce0c32a9cd9d54
Fuka59 <fukaneville559@gmail.com>
2023-05-16 09:01:52 +0100

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+reactDom.render(<APP/> , document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+reactDom.render(<APP/> , document.getElementById('root')); 
+
